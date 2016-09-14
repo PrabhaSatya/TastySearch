275194e89095e4e5987fa8855ed7c65e0841c9b9
prabhasatya <satya.139@gmail.com>
2016-09-14 18:28:15 +0530

    Added code to exclude given stopwords Added code to create an inverted index from the documents parsed.

--- src/Main.java
+++ src/Main.java
@@ -8,6 +8,10 @@ public class Main {
     public static void main(String args[]) throws IOException {
         ParseInput parseInput = new ParseInput();
         Map<Integer,Document> documents = parseInput.getDocuments("inputfiles/finefoods.txt",0.2);
+        System.out.println("Total documents:"+documents.size());
+        Set<String> stopWords = parseInput.getStopWords("inputfiles/stopwords");
+        System.out.println("Total stopwords:"+stopWords.size());
+        Map<String,List<Integer>> index = parseInput.getIndex(documents,stopWords);
+        System.out.println("Unique words in the input sample(without stopwords):"+index.size());
     }
-
 }
--- src/ParseInput.java
+++ src/ParseInput.java
@@ -1,17 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by Prabha on 14/09/16.
  */
 class ParseInput {
 
-    Map<Integer, Document> getDocuments(String inputFile, double sampleRate) throws IOException {
-
+    Map<Integer,Document> getDocuments(String inputFile, double sampleRate) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new FileReader(inputFile));
         Map<Integer, Document> documents = new HashMap();
         int skipLines = 4;
@@ -19,7 +16,6 @@ class ParseInput {
         String line;
         Integer documentId = 0;
         Integer parseDocuments = 0;
-
         while (null != (line = bufferedReader.readLine())) {
             if (line.length() == 0) {
                 parseDocuments++;
@@ -44,5 +40,51 @@ class ParseInput {
         }
         return documents;
 
+    }
+
+     Set<String> getStopWords(String stopWordsFile) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(stopWordsFile));
+        Set<String> stopWords = new HashSet<>();
+        String line;
+        while(null != (line = bufferedReader.readLine())){
+            if(!stopWords.contains(line)){
+                stopWords.add(line);
+            }
+        }
+        return stopWords;
+    }
+
+    Map<String,List<Integer>> getIndex(Map<Integer, Document> documents, Set<String> stopWords) {
+        Map<String,List<Integer>> index = new HashMap<>();
+        for(Map.Entry<Integer, Document> document:documents.entrySet()){
+            parseDocument(index, document,stopWords);
+        }
+        return index;
+    }
+
+    private void parseDocument(Map<String, List<Integer>> index, Map.Entry<Integer, Document> document,Set<String> stopWords) {
+        Integer documentId = document.getValue().id;
+        String summary = document.getValue().summary;
+        String text = document.getValue().text;
+        String[] summaryWords = summary.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+        insertWordsToIndex(index, documentId, summaryWords,stopWords);
+        String[] textWords = text.replaceAll("[^a-zA-Z ]", "").toLowerCase().split("\\s+");
+        insertWordsToIndex(index,documentId,textWords,stopWords);
+    }
+
+    private void insertWordsToIndex(Map<String, List<Integer>> index, Integer documentId, String[] summaryWords,Set<String> stopWords) {
+        for(String word:summaryWords){
+            if(!stopWords.contains(word)){
+                if(null == index.get(word)){
+                    List<Integer> documentList = new ArrayList<>();
+                    documentList.add(documentId);
+                    index.put(word, documentList);
+                }else{
+                    if(!index.get(word).contains(documentId)){
+                        index.get(word).add(documentId);
+                    }
+                }
+            }
+        }
     }
 }
